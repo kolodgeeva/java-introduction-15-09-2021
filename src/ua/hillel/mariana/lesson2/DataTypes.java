3680c091cff50ff5f68459dd9e7e3b472606ba92
mariana <kolodgeeva@gmail.com>
2021-09-18 18:21:15 +0300

    Initial commit

---
+++ src/ua/hillel/mariana/lesson2/DataTypes.java
@@ -0,0 +1,40 @@
+package ua.hillel.mariana.lesson2;
+
+public class DataTypes {
+
+  public static void main(String[] args) {
+
+    byte byteVariable = -50;
+    System.out.println("byteVariable: " + byteVariable);
+
+    short shortVariable = 32_000;
+    System.out.println("shortVariable: " + shortVariable);
+
+    int intVariable = 2_000_000_000;
+    System.out.println("intVariable: " + intVariable);
+
+    long longVariable = 2_000_000_000_000L;
+    System.out.println("longVariable: " + longVariable);
+
+    float floatVariable = 34.5676F;
+    System.out.println("floatVariable: " + floatVariable);
+
+    double doubleVariable = 34.567656765;
+    System.out.println("doubleVariable: " + doubleVariable);
+
+    boolean booleanVariable = true; // false
+    System.out.println("booleanVariable: " + booleanVariable);
+
+    char charVariable = 'р';
+    System.out.println("charVariable: " + charVariable);
+
+    char charNumberVariable = 50000;
+    System.out.println("charNumberVariable: " + charNumberVariable);
+
+    String stringVariable = "some text";
+    System.out.println("stringVariable: " + stringVariable);
+
+
+  }
+
+}
---
+++ src/ua/hillel/mariana/lesson2/Main.java
@@ -0,0 +1,24 @@
+package ua.hillel.mariana.lesson2;
+
+public class Main {
+
+  /*
+  Многострочный коментарий
+  Shift + F6 - переименовать
+  Shift + F10 - запустить
+  psvm - create main method
+  sout - System.out.println
+   */
+
+  // Однострочный коментарий
+
+  /**
+   * Java doc comment
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.print("new text");
+    System.out.println("Hello!");
+  }
+
+}
